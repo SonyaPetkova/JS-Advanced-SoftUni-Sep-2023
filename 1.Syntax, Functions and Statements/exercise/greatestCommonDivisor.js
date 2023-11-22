4616a857dbc9ccc98c4c5f9ba59daf586856b300
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-11-22 15:53:49 +0100

    Previous Day
    Function that calculates the date of the previous day by given year, month, and day.

--- 1.Syntax, Functions and Statements/exercise/greatestCommonDivisor.js
+++ 1.Syntax, Functions and Statements/exercise/greatestCommonDivisor.js
@@ -1,15 +1,14 @@
-function greatestCommonDivisor(num1,num2) {
-    let result = Math.min(num1, num2);
-    while (result > 0) {
-        if (num1 % result == 0 && num2 % result == 0) {
-            break;
-        }
-        result--;
+function greatestCommonDivisor(num1, num2) {
+  let result = Math.min(num1, num2);
+  while (result > 0) {
+    if (num1 % result == 0 && num2 % result == 0) {
+      break;
     }
+    result--;
+  }
 
-    console.log(result);
+  console.log(result);
 }
 
-
-greatestCommonDivisor(15, 5)
-greatestCommonDivisor(2154, 458)
+greatestCommonDivisor(15, 5);
+greatestCommonDivisor(2154, 458);
---
+++ 1.Syntax, Functions and Statements/exercise/previousDay.js
@@ -0,0 +1,12 @@
+function previousDay(year, month, day) {
+  const previousDay = new Date(year, month - 1, day - 1);
+  console.log(previousDay);
+
+  console.log(
+    `${previousDay.getFullYear()}-${
+      previousDay.getMonth() + 1
+    }-${previousDay.getDate()}`
+  );
+}
+previousDay(2016, 9, 30);
+previousDay(2015, 5, 10);
