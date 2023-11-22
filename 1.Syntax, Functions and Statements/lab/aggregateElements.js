c023730ff743841a1f6254eb20b8a8d20ffb4c5c
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-11-22 12:51:50 +0100

    Aggregate Elements
    Program that performs different operations on an array of elements:
    · Sum- calculates the sum of all elements
    · Sum- calculates the sum of the inverse values of all elements
    · Concat-concatenates the string representations of all elements

---
+++ 1.Syntax, Functions and Statements/lab/aggregateElements.js
@@ -0,0 +1,13 @@
+function aggregateElements(input) {
+  let arrAsString = input.map((el) => el.toString()).reduce((a, b) => a + b);
+
+  let sum = input.reduce((a, b) => a + b);
+
+  let sumInverseValues = input.map((el) => 1 / el).reduce((a, b) => a + b);
+
+  console.log(sum);
+
+  console.log(sumInverseValues);
+  console.log(arrAsString);
+}
+aggregateElements([1, 2, 3]);
