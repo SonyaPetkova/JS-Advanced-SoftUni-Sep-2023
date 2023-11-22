633018476dbcf9049e37a622757a56210402c584
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-11-22 11:26:13 +0100

    Days in a month task
    Function getting the number of days in a month.

---
+++ 1.Syntax, Functions and Statements/lab/daysInAMonth.js
@@ -0,0 +1,6 @@
+function daysInAMonth(month,year) {
+    let result= new Date(year,month,0).getDate();
+  return result
+    
+}
+daysInAMonth(2, 2021)
--- 1.Syntax, Functions and Statements/lab/daysOfWeek.js
+++ 1.Syntax, Functions and Statements/lab/daysOfWeek.js
@@ -1,34 +1,34 @@
 function daysOfWeek(input) {
-    result=0;
+  result = 0;
   switch (input) {
     case "Monday":
-      result=1;
+      result = 1;
       break;
     case "Tuesday":
-      result=2;
+      result = 2;
       break;
     case "Wednesday":
-      result=3;
+      result = 3;
       break;
     case "Thursday":
-      result=4;
+      result = 4;
       break;
     case "Friday":
-      result=5;
+      result = 5;
       break;
     case "Saturday":
-      result=6;
+      result = 6;
       break;
     case "Sunday":
-      result=7;
+      result = 7;
       break;
 
     default:
-        result="error";
+      result = "error";
       break;
   }
-  console.log(result);
-  return result
+
+  return result;
 }
 daysOfWeek("Monday");
 daysOfWeek("Invalid");
