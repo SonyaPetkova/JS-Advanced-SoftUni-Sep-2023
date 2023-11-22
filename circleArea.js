f0538e1a87b572bf6e7abbca5530ab9716cf4c85
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-11-22 10:40:45 +0100

    Math Operations task
    Receiving three argument: 2 numbers and a operator. Print the output on the console.

--- circleArea.js
+++ circleArea.js
@@ -1,15 +1,13 @@
 function circleArea(input) {
-    let result=0;
+  let result = 0;
   if (typeof input != "number") {
     console.log(
       `We can not calculate the circle area, because we receive a ${typeof input}.`
     );
-  }else{
-    result=Math.PI*Math.pow(input,2)
-console.log(result.toFixed(2));
+  } else {
+    result = Math.PI * Math.pow(input, 2);
+    console.log(result.toFixed(2));
   }
-
-
 }
 circleArea(5);
 circleArea("name");
