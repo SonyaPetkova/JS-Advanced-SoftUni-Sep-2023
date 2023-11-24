22928c3f8200a337b0542e576e36d265b7700b10
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-11-24 10:24:39 +0100

    Sum First Last task
    sum the first and the last numbers in an array and return the output

--- 2.Arrays and Nested Arrays/lab/lastKNumbersSequence.js
+++ 2.Arrays and Nested Arrays/lab/lastKNumbersSequence.js
@@ -2,13 +2,20 @@ function lastKNumbersSequence(n, k) {
   let result = [1];
   let sumArr = [1];
   let currentNumber = 0;
- 
+  // until the length of the result is less than the given n length
   for (let i = 0; result.length <= n - 1; i++) {
+    //check if we have less than the given elements
+    //if we have less sum all elements
+
     if (result.length <= k) {
       currentNumber = result.reduce((a, b) => a + b);
       result.push(currentNumber);
       sumArr.push(currentNumber);
     } else {
+      // if we have more sum the last k elements
+      //We add always 1 number and extract 1 number from the sum Arr
+      //this way we always have the given k elements from the input
+
       sumArr.shift();
       currentNumber = sumArr.reduce((a, b) => a + b);
 
@@ -16,7 +23,7 @@ function lastKNumbersSequence(n, k) {
       result.push(currentNumber);
     }
   }
-  console.log(result);
+  return result;
 }
 
 lastKNumbersSequence(6, 3);
---
+++ 2.Arrays and Nested Arrays/lab/sumFirstLast.js
@@ -0,0 +1,7 @@
+function sumFirstLast(arr) {
+    let firstNumber=Number(arr.shift())
+    let lastNumber=Number(arr.pop())
+    let result=firstNumber+lastNumber
+    console.log(result);
+}
+sumFirstLast(['20', '30', '40'])
