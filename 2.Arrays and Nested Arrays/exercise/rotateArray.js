49ef09f9069f48431b1f92f6f5673134b05a1757
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-11-28 17:04:58 +0100

    Rotate Array
    The array should be rotated to the right side, meaning that the last element should become the first, upon rotation.
    
    The input comes as two parameters – an array of strings and a number. The second parameter is the amount of rotation you need to perform.
    The output is the resulting array after the rotations. The elements should be printed on one line, separated by a single space.

---
+++ 2.Arrays and Nested Arrays/exercise/rotateArray.js
@@ -0,0 +1,16 @@
+function rotateArray(arr, num) {
+  let result = arr;
+  for (let i = 0; i < num; i++) {
+    let currentElement = result.pop();
+    result.unshift(currentElement);
+    
+  }
+  console.log(result.join(" "));
+}
+rotateArray(
+  ["1", "2", "3", "4"],
+
+  2
+);
+
+rotateArray(['Banana', 'Orange', 'Coconut', 'Apple'], 15)
