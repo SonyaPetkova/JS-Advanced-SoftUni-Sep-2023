40a129f38c10d6518da661964fc117e8afc7578b
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-12-01 15:47:19 +0100

    City Taxes Task
    This task is an extension of Problem 1.
    You will receive a city’s name (string), population (number), and treasury (number) as arguments, which you will need to set as properties of an object and return it. In addition to the input parameters, the object must have a property taxRate with an initial value of 10, and three methods for managing the city:
    
    · collectTaxes() - Increase treasury by population * taxRate
    
    · applyGrowth(percentage) - Increase population by given percentage
    
    · applyRecession(percentage) - Decrease treasury by given percentage
    
    Round down the values after each calculation.

--- 3.Objects and Compositions/lab/townPopulation.js
+++ 3.Objects and Compositions/lab/townPopulation.js
@@ -1,39 +1,40 @@
 function townPopulation(townsArr) {
-    const towns={};
-    for (let townAsString of townsArr) {
-        // Split each string into 'name' and 'population' using ' <-> ' as the separator
-        let [name, population] = townAsString.split(' <-> ');
-        population = Number(population);
-        // Check if the town 'name' already exists in the 'towns' object
-        if (towns[name] != undefined) {
-        // If the town exists, add the 'population' to the existing value in 'towns'
-        population += towns[name];
-        }
-        // Update the 'towns' object with the new 'population' value for the 'name'
-        towns[name] = population;
-        }
-        for (let town in towns) {
-        console.log(`${town} : ${towns[town]}`);
-        }
-        }
+  const towns = {};
+  for (let townAsString of townsArr) {
+    let [name, population] = townAsString.split(" <-> ");
+    population = Number(population);
 
+    if (towns[name] != undefined) {
+      population += towns[name];
+    }
 
-townPopulation(['Sofia <-> 1200000',
+    towns[name] = population;
+  }
+  for (let town in towns) {
+    console.log(`${town} : ${towns[town]}`);
+  }
+}
 
-'Montana <-> 20000',
+townPopulation([
+  "Sofia <-> 1200000",
 
-'New York <-> 10000000',
+  "Montana <-> 20000",
 
-'Washington <-> 2345000',
+  "New York <-> 10000000",
 
-'Las Vegas <-> 1000000'])
+  "Washington <-> 2345000",
 
-townPopulation(['Istanbul <-> 100000',
+  "Las Vegas <-> 1000000",
+]);
 
-'Honk Kong <-> 2100004',
+townPopulation([
+  "Istanbul <-> 100000",
 
-'Jerusalem <-> 2352344',
+  "Honk Kong <-> 2100004",
 
-'Mexico City <-> 23401925',
+  "Jerusalem <-> 2352344",
 
-'Istanbul <-> 1000'])
+  "Mexico City <-> 23401925",
+
+  "Istanbul <-> 1000",
+]);
