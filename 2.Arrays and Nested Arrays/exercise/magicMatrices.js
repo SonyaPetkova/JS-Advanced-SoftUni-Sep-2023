03eb91c25e47abb18c7b597ca8a31e8e207cf85c
SonyaPetkova <sonya.petkovaa@gmail.com>
2023-12-01 12:46:24 +0100

    City Record
    Set as properties of an object and return it.

--- 2.Arrays and Nested Arrays/exercise/magicMatrices.js
+++ 2.Arrays and Nested Arrays/exercise/magicMatrices.js
@@ -3,22 +3,17 @@ function magicMatrices(input) {
   let sumColumn = 0;
   let isValid = true;
 
-  function isVAlid(num1,num2){
-    if (num1 != num2) {
-        isValid = false;
-      } else {
-        isValid = true;
-      }
-      
-  }
-
   for (let i = 0; i < input.length; i++) {
     let row = input[i];
 
     let currentSumRow = row.reduce((a, b) => a + b);
 
-   isVAlid(currentSumRow,sumRow);
-   sumRow=currentSumRow;
+    if (currentSumRow != sumRow) {
+      isValid = false;
+    } else {
+      isValid = true;
+    }
+    sumRow = currentSumRow;
 
     let columnArr = [];
 
@@ -26,23 +21,37 @@ function magicMatrices(input) {
       let column = input[j][i];
       columnArr.push(column);
     }
-    let currentColumnSum = columnArr.reduce((a,b)=>a+b);
-    
- isVAlid(currentColumnSum,sumColumn) 
+    let currentColumnSum = columnArr.reduce((a, b) => a + b);
 
-      sumColumn = currentColumnSum;
-    
-      isVAlid(currentColumnSum,currentSumRow)
-      
-      
+    if (currentColumnSum != sumColumn) {
+      isValid = false;
+    } else {
+      isValid = true;
+    }
+    sumColumn = currentColumnSum;
+
+    if (currentColumnSum != currentSumRow) {
+      isValid = false;
+    }
   }
   console.log(isValid);
 }
 
-magicMatrices([[4, 5, 6],
 
-    [6, 5, 4],
-    
-    [5, 5, 5]])
-magicMatrices([[11, 32, 45], [21, 0, 1], [21, 1, 1]])
-magicMatrices([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
+magicMatrices([
+  [4, 5, 6],
+
+  [6, 5, 4],
+
+  [5, 5, 5],
+]);
+magicMatrices([
+  [11, 32, 45],
+  [21, 0, 1],
+  [21, 1, 1],
+]);
+magicMatrices([
+  [1, 0, 0],
+  [0, 0, 1],
+  [0, 1, 0],
+]);
---
+++ 3.Objects and Compositions/lab/cityRecord.js
@@ -0,0 +1,10 @@
+function cityRecord(city,population,treasury) {
+   return {
+    city,population,treasury
+   }
+}
+console.log(cityRecord('Tortuga',
+
+7000,
+
+15000));
