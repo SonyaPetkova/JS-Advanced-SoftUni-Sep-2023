92d198f6c36e8bfd91f2d755d9c2ab7ce4c973c3
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-03 19:30:57 +0100

    Collect List Items
    Write a JS function that scans a given HTML list and appends all collected list items’ text to a textarea on the same page when the user clicks on a button.
    
    Input/Output
    
    There will be no input/output, your program should instead modify the DOM of the given HTML document.

--- DOM Introduction/lab/02.Collect List Items/CollectListItems.js
+++ DOM Introduction/lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+  let items = document.querySelectorAll("ul#items li");
+
+  let textArea = document.querySelector("#result");
+  for (let el of items) {
+    console.log(el);
+    console.log(typeof el);
+    textArea.value += el.textContent + "\n";
+  }
 }
