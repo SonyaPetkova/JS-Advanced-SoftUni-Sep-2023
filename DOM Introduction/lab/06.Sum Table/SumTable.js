89bd4b1fe01dffcd1822331f2154a1b3c9c4c7ce
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-04 14:17:28 +0100

    Sum Table
    Write a JS function that finds the first table in a document and sums the values in the last column. The result is then displayed in an element with ID "sum".
    
    Input/Output
    
    There will be no input/output, your program should instead modify the DOM of the given HTML document.

--- DOM Introduction/lab/06.Sum Table/SumTable.js
+++ DOM Introduction/lab/06.Sum Table/SumTable.js
@@ -1,3 +1,9 @@
 function sumTable() {
-
+  let table = document.querySelectorAll("table td:nth-child(2)");
+  let sum = 0;
+  for (const el of table) {
+    let current = Number(el.textContent);
+    sum += current;
+  }
+  document.querySelector("#sum").textContent=sum
 }
