5ae5724b0e71a872998e9d2a2b8ad5f1c4c90135
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-15 14:45:46 +0100

    Search List task
    An HTML page holds a list of towns, a search box, and a [Search] button. Implement the search function to bold and underline the items from the list which include the text from the search box. Also, print the number of items the current search matches in the format `${matches} matches found`.

--- DOM Introduction/exercise/04. Search in List/search.js
+++ DOM Introduction/exercise/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+  // TODO
+  const towns = document.querySelectorAll("#towns li");
+
+  let searchText = document.getElementById("searchText").value;
+
+  let counter=0;
+
+  for (const el of towns) {
+    let town = el.textContent.toLowerCase();
+    if (town.includes(searchText.toLowerCase())) {
+      el.style.fontWeight = "bold";
+      el.style.textDecoration = "underline";
+      counter++;
+    }else{
+      el.style.fontWeight = "";
+      el.style.textDecoration = "none";
+    }
+   
+   
+  }
+  document.getElementById("result").textContent=`${counter} matches found`
+ 
 }
