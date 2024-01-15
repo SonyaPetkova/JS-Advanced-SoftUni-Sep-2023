bd8acd94c0cb3c7d53a641e1a8e710a1df77d715
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-15 11:46:05 +0100

    Subtraction and Pascal or Camel case tasks
    1. Subtraction
    
    An HTML page holds two text fields with ids "firstNumber" and "secondNumber". Write a function to subtract the values from these text fields and display the result in the div named "result".
    2. Pascal or Camel Case
    
    An HTML file is given and your task is to write a function that takes two string parameters as an input and transforms the first parameter to the type required by the second parameter.
    
    · The first parameter will be the text that you need to modify depending on the second parameter. The words in it will always be separated by space.
    
    · The second parameter will be either "Camel Case" or "Pascal Case". In case of different input, your output should be "Error!

--- DOM Introduction/exercise/01. Subtraction/subtract.js
+++ DOM Introduction/exercise/01. Subtraction/subtract.js
@@ -1,9 +1,6 @@
 function subtract() {
-let num1=document.querySelector("#firstNumber").value;
-let num2=document.querySelector("#secondNumber").value;
-let sum=Number(num1)-Number(num2);
-document.querySelector("#result").textContent=sum
-
-
-   
+  let num1 = document.querySelector("#firstNumber").value;
+  let num2 = document.querySelector("#secondNumber").value;
+  let sum = Number(num1) - Number(num2);
+  document.querySelector("#result").textContent = sum;
 }
--- DOM Introduction/exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction/exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,38 @@
 function solve() {
   //TODO...
+  let text = document.getElementById("text").value;
+  let convention = document.getElementById("naming-convention").value;
+  let textArr = text.toLowerCase().split(" ");
+  let result = [];
+
+  switch (convention) {
+    case "Camel Case":
+      result.push(textArr[0]);
+      for (let i = 1; i < textArr.length; i++) {
+        let currentWord = textArr[i];
+        let wordCamelCase = currentWord[0];
+        wordCamelCase = wordCamelCase.toUpperCase();
+        wordCamelCase += currentWord.substring(1);
+
+        result.push(wordCamelCase);
+      }
+      document.getElementById("result").textContent = result.join("");
+
+      break;
+
+    case "Pascal Case":
+      for (const currentWord of textArr) {
+        let wordPascalCase = currentWord[0];
+        wordPascalCase = wordPascalCase.toUpperCase();
+        wordPascalCase += currentWord.substring(1);
+        result.push(wordPascalCase);
+      }
+      document.getElementById("result").textContent = result.join("");
+
+      break;
+
+    default:
+      document.getElementById("result").textContent="Error!";
+      break;
+  }
 }
