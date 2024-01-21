050b5afc983e52cb22ab075850ad2b990fd3168e
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-21 15:01:30 +0100

    Hell's Kitchen updated
    -the logic for the sum of average salary was wrong , when there were 2 restaurants with the same name
    - the logic for best Salary was also wrong , when there were 2 restaurants with the same name

--- DOM Introduction/exercise/07. Hell's Kitchen/app.js
+++ DOM Introduction/exercise/07. Hell's Kitchen/app.js
@@ -6,38 +6,35 @@ function solve() {
     let input = document.getElementsByTagName("textarea")[0].value;
     let inputArr = JSON.parse(input);
     let result = {};
-    
 
     for (const el of inputArr) {
-      let average = 0;
-      let bestSalary = 0;
-
       let [restaurantName, data] = el.split(" - ");
       data.split(", ").forEach((element) => {
         let [worker, salary] = element.split(" ");
-        average += Number(salary);
 
-        if (Number(salary) > bestSalary) {
-          bestSalary = salary;
-        }
         if (!result.hasOwnProperty(`${restaurantName}`)) {
           result[restaurantName] = {};
           result[restaurantName].workersData = [];
+          result[restaurantName]["Average Salary"] = 0;
+          result[restaurantName]["Best Salary"] = 0;
         }
-        let workersData=worker +" "+ salary;
-        result[restaurantName].workersData.push(workersData)
+        if (Number(salary) > result[restaurantName]["Best Salary"]) {
+          result[restaurantName]["Best Salary"] = salary;
+        }
+        result[restaurantName]["Average Salary"] += Number(salary);
+        //
+        let workersData = worker + " " + salary;
 
-        
-        result[restaurantName]["Average Salary"] =
-          average / Object.keys(result[restaurantName].workersData).length;
+        result[restaurantName].workersData.push(workersData);
       });
-      result[restaurantName]["Best Salary"] = Number(bestSalary);
     }
 
     let currentAverage = 0;
     let bestRestaurant = "";
-    let lastOneAdded="";
+    let lastOneAdded = "";
     for (const key in result) {
+      result[key]["Average Salary"] =
+        result[key]["Average Salary"] / Number(result[key].workersData.length);
       if (result[key]["Average Salary"] === currentAverage) {
         bestRestaurant = lastOneAdded;
       }
@@ -46,35 +43,29 @@ function solve() {
 
         bestRestaurant = key;
       }
-    
-      lastOneAdded=key;
+      lastOneAdded = key;
     }
-
 
     let output1 = `Name: ${bestRestaurant} Average Salary: ${result[
       bestRestaurant
-    ]["Average Salary"].toFixed(2)} Best Salary: ${result[bestRestaurant][
-      "Best Salary"
-    ].toFixed(2)}`;
+    ]["Average Salary"].toFixed(2)} Best Salary: ${Number(
+      result[bestRestaurant]["Best Salary"]
+    ).toFixed(2)}`;
     document.querySelector("#bestRestaurant p").textContent = output1;
+
     let output2 = [];
-  
+    for (let el of result[bestRestaurant].workersData) {
+      [worker, salary] = el.split(" ");
 
+      output2.push([worker, salary]);
+    }
 
-for (let el of result[bestRestaurant].workersData) {
-
-[worker,salary]=el.split(" ");
-
-output2.push([worker,salary])
-}
-
-let sortedOutput=output2.sort((a,b)=>b[1]-a[1]);
-console.log(sortedOutput);
-let sortedOutput2="";
-    for (const [name, salary] of sortedOutput){
-    sortedOutput2 += `Name: ${name} With Salary: ${salary} `;
+    let sortedOutput = output2.sort((a, b) => b[1] - a[1]);
+    console.log(sortedOutput);
+    let sortedOutput2 = "";
+    for (const [name, salary] of sortedOutput) {
+      sortedOutput2 += `Name: ${name} With Salary: ${salary} `;
     }
     document.querySelector("#workers p").textContent = sortedOutput2;
-  console.log(sortedOutput2);
   }
 }
