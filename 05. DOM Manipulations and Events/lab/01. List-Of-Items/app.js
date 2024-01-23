218419fb00328d8a5d6fe6c93f9d76d8bae2f996
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-23 11:00:33 +0100

    Delete from table Task
    Write a program that takes an email from an input field and deletes the matching row from a table.
    
    · If entry is found, the textContent in the element with id="result" must be set to "Deleted."
    
    · Otherwise, an error should be displayed in a <div> with id="result". The error should be "Not found."
    
    Submit only the deleteByEmail() function in Judge.

--- 05. DOM Manipulations and Events/lab/01. List-Of-Items/app.js
+++ 05. DOM Manipulations and Events/lab/01. List-Of-Items/app.js
@@ -1,15 +1,3 @@
-// function addItem() {
-//     console.log('TODO:...');
-//     const text=document.querySelector("#newItemText").value;
-//     const li=document.createElement("li");
-//     console.log(li);
-//     li.appendChild(document.createTextNode(text));
-//     document.getElementById("items").appendChild("li");
-
-
-    
-// }
-
 function addItem() {
     let text = document.getElementById('newItemText').value;
     let li = document.createElement("li");
--- 05. DOM Manipulations and Events/lab/02. Delete-From-Table/app.js
+++ 05. DOM Manipulations and Events/lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  console.log("TODO:...");
+  const inputValue = document.getElementsByName("email")[0].value;
+  const mailTable = document.querySelectorAll("#customers tr td:nth-child(2)");
+
+  for (const email of mailTable) {
+    let result = document.getElementById("result");
+    if (inputValue === email.textContent) {
+      let row = email.parentNode;
+      row.parentNode.removeChild(row);
+      result.textContent = "Deleted.";
+      console.log("deleted");
+      return
+    }
+   document.getElementById("result").textContent="Not found."
+  }
 }
