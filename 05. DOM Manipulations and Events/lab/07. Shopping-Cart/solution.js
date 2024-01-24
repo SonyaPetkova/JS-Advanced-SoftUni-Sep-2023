86e6da8783c1287faaf0a924f4c21fb56021bf44
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-24 10:34:52 +0100

    7. Shopping Cart
    You will be given some products that you should be able to add to your cart. Each product will have a name, picture, and price.
    
    When the "Add" button is clicked, append the current product to the textarea in the following format: "Added {name} for {money} to the cart.\n". The price must be fixed to the second digit.
    
    When the button "Checkout" is clicked, calculate the total money that you need to pay for the products that are currently in your cart. Append the result to the textarea in the following format:
    
    "You bought {list} for {totalPrice}."
    
    The list should contain only the unique products, separated by ", ". The total price should be rounded to the second decimal point.
    
    Also, after clicking over "Checkout" and every from above is done you should disable all buttons. (You can't add products or checkout again if once the checkout button is clicked).

--- 05. DOM Manipulations and Events/lab/07. Shopping-Cart/solution.js
+++ 05. DOM Manipulations and Events/lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-   //TODO...
+
+  let addButtons = document.querySelectorAll(".add-product");
+  let productsBought = [];
+  let totalPrice = 0;
+  let textArea = document.querySelector("textarea");
+  let checkoutBtn = document.querySelector(".checkout");
+
+  for (const addBtn of addButtons) {
+    addBtn.addEventListener("click", addProduct);
+  }
+  checkoutBtn.addEventListener("click", checkOut);
+
+  function addProduct(e) {
+    let productInfo = e.target.parentNode.parentNode;
+    let productTitle = productInfo.querySelector(".product-title").textContent;
+    let productPrice = productInfo.querySelector(
+      ".product-line-price"
+    ).textContent;
+
+    if (!productsBought.includes(productTitle)) {
+      productsBought.push(productTitle);
+    }
+
+    totalPrice += Number(productPrice);
+    let message = `Added ${productTitle} for ${Number(productPrice).toFixed(
+      2
+    )} to the cart.\n`;
+    textArea.value += message;
+  }
+
+  function checkOut(e) {
+     
+      textArea.value += `You bought ${productsBought.join(
+        ", "
+      )} for ${totalPrice.toFixed(2)}.`;
+      for (const btn of addButtons) {
+        console.log(btn);
+        btn.disabled = true;
+      }
+      checkoutBtn.disabled = true;
+      console.log(checkoutBtn);
+    
+  }
 }
