c5d2719fc31fb829bc998a5c3278f4d226fd2435
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-25 13:17:44 +0100

    5. Encode and Decode Messages
    In this problem, you should create a JS functionality that encodes and decodes some messages which travel to
    
    the network.
    This program should contain two functionalities.
    
    The first one is to encode the given message and send it to the receiver.
    
    The second one is to decode the received message and read it (display it).
    
    When the [Encode and send it] button is clicked, you should get the given message from the first textarea. When you get the current message, you should encode it as follows:
    
    · Change the ASCII CODE on every single character in that message when you add 1 to the current ASCII NUMBER, that represents the current character in that message
    
    · Clear the sender textarea and add the encoded message to the receiver textarea
    After that, when the [Decode and read it] button is clicked. You need to get the encoded message from the receiver textarea and do the opposite logic from encoding:
    
    · Subtract 1 from the current ASCII NUMBER, that represents the current character in that message
    
    · Replace the encoded message with the already decoded message in the receiver textarea, to make it readable

--- 05. DOM Manipulations and Events/exercise/04. Fill-Dropdown/dropdown.js
+++ 05. DOM Manipulations and Events/exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+  const text = document.getElementById("newItemText");
+  const value = document.getElementById("newItemValue");
+
+  const menuRef = document.getElementById("menu");
+  const option = document.createElement("option");
+  option.textContent = text.value;
+  option.value = value.value;
+  menuRef.appendChild(option);
+  text.value = "";
+  value.value = "";
 }
--- 05. DOM Manipulations and Events/exercise/05. Encode-and-Decode-Messages/app.js
+++ 05. DOM Manipulations and Events/exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,45 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  console.log("TODO...");
+  const encodeAreaRef = document.querySelectorAll("div textarea")[0];
+  const decodeAreaRef = document.querySelectorAll("div textarea")[1];
+  const encodeBtn = document.querySelectorAll("button")[0];
+  const decodeBtn = document.querySelectorAll("button")[1];
+
+  encodeBtn.addEventListener("click", encodeHandler);
+  decodeBtn.addEventListener("click", decodeHandler);
+
+  function encodeHandler(e) {
+    let current = e.target.parentNode;
+    let message = current.querySelector("textarea").value;
+    message = encode(message);
+
+    decodeAreaRef.value = message;
+    encodeAreaRef.value = "";
+  }
+  function encode(message) {
+    let charsArr = message.split("");
+    let encodeMsg = [];
+    charsArr.forEach((element) => {
+      let nextAscIINum = element.charCodeAt() + 1;
+      let encodedChar = String.fromCharCode(nextAscIINum);
+      encodeMsg.push(encodedChar);
+    });
+    return encodeMsg.join("");
+  }
+  function decodeHandler(e) {
+    let current = e.target.parentNode;
+    let message = current.querySelector("textarea").value;
+    message = decode(message);
+    decodeAreaRef.value = message;
+  }
+  function decode(message) {
+    let charsArr = message.split("");
+    let encodeMsg = [];
+    charsArr.forEach((element) => {
+      let nextAscIINum = element.charCodeAt() - 1;
+      let encodedChar = String.fromCharCode(nextAscIINum);
+      encodeMsg.push(encodedChar);
+    });
+    return encodeMsg.join("");
+  }
 }
