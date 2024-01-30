35d90bf7dd1226d8d71b7e35625d94bdbe67c0ab
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-30 15:17:05 +0100

    7. Cars
    Write a closure that can create and modify objects. All created objects should be kept and be accessible by name. You should support the following functionality:
    
    · create <name> - creates an object with the supplied <name>
    
    · create <name> inherits <parentName> - creates an object with the given <name>, that inherits from the parent object with the <parentName>
    
    · set <name> <key> <value> - sets the property with key equal to <key> to <value> in the object with the supplied <name>.
    
    · print <name> - prints the object with the supplied <name> in the format "<key1>:<value1>,<key2>:<value2>…" - the printing should also print all inherited properties from parent objects. Inherited properties should come after own properties.
    
    Input
    
    The input will come as an array of strings - each string represents a command to be executed from your closure.
    
    Output
    
    For every print command - you should print on the console all properties of the object in the above-mentioned format.
    
    Constraints
    
    · All commands will always be valid, there will be no nonexistent or incorrect input.
    PROBLEM WITH THE INHERITANCE ... TO BE FIXED

---
+++ 06.Advanced Functions/lab/cars.js
@@ -0,0 +1,58 @@
+function createCar(inputArr) {
+  let cars = [];
+
+  function create(...arg) {
+    if (!arg.includes("inherit")) {
+        let [newCarName] = [...arg];
+    cars[newCarName] = {};
+      
+    } else {
+      let [newCarName, _, proto] = [...arg];
+      
+      cars[newCarName] = Object.create(cars[proto]);
+    }
+  }
+  function set(...arg) {
+    let [obj, key, value] = [...arg];
+    
+
+    cars[obj][key]= value ;
+  }
+
+  function print(obj) {
+    let result = [];
+    Object.entries(cars[obj]).forEach((el) => {
+      result.push(`${el[0]}:${el[1]}`);
+    });
+    console.log(result.join(","));
+  }
+  inputArr.forEach((element) => {
+    let [command, ...args] = element.split(" ");
+
+    switch (command) {
+      case "create":
+        create(...args);
+        break;
+      case "set":
+        return set(...args);
+      case "print":
+        return print(...args);
+    }
+  });
+}
+
+
+
+createCar([
+  "create c1",
+
+  "create c2 inherit c1",
+
+  "set c1 color red",
+
+  "set c2 model new",
+
+  "print c1",
+
+  "print c2",
+]);
---
+++ 06.Advanced Functions/lab/listProcessor.js
@@ -0,0 +1,35 @@
+function solve(input) {
+  let result = [];
+  const processObj = {
+    add: (el) => {
+      result.push(el);
+    },
+    remove: (el) => {
+      result.forEach((text) => {
+        if (el === text) {
+          result.splice(result.indexOf(el), 1);
+        }
+      });
+    },
+    print: () => {
+      console.log(result.join(","));
+    },
+  };
+
+  input.forEach((element) => {
+    let [command, text] = element.split(" ");
+    if (command === "print") {
+      processObj.print();
+    }
+    if (command === "add") {
+      processObj.add(text);
+    }
+    if (command === "remove") {
+      processObj.remove(text);
+    }
+
+    
+  });
+}
+
+solve(["add hello", "add again", "remove hello", "add again", "print"]);
