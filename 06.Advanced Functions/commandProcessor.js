f1a12bb55627480c2f5bd6348fe9042536a8245f
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-01-30 09:45:16 +0100

    5. Command Processor
    Write a program that keeps a string inside its context and can execute different commands that modify or print the string on the console.
    
    append(string) - append the given string at the end of the internal string
    
    removeStart(n) - remove the first n characters from the string, n is an integer
    
    removeEnd(n) - remove the last n characters from the string, n is an integer
    
    print - print the stored string on the console

---
+++ 06.Advanced Functions/commandProcessor.js
@@ -0,0 +1,31 @@
+function solution() {
+  let internalStr = "";
+  return {
+    append: (str) => {
+      return (internalStr += str);
+    },
+    removeStart: (num) => {
+      return (internalStr = internalStr.substring(num));
+    },
+    removeEnd: (num) => {
+      return (internalStr = internalStr.substring(0, internalStr.length-num));
+    },
+    print: () => {
+      return console.log(internalStr);
+    },
+  };
+
+}
+let firstZeroTest = solution();
+firstZeroTest.append('hello'); firstZeroTest.append('again'); firstZeroTest.removeStart(3); firstZeroTest.removeEnd(4); firstZeroTest.print();
+let secondZeroTest = solution();
+
+secondZeroTest.append('123');
+
+secondZeroTest.append('45');
+
+secondZeroTest.removeStart(2);
+
+secondZeroTest.removeEnd(1);
+
+secondZeroTest.print();
