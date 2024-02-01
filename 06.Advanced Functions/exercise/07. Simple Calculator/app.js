20f0c56adbc0519eb756b30eaa80918487dd0cbc
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-01 13:01:30 +0100

    Update app.js
    7. Simple Calculator
    
    Create a function calculator which returns an object that can modify the DOM. The returned object should support the following functionality:
    
    · init (selector1, selector2, resultSelector) - initializes the object to work with the elements corresponding to the supplied selectors.
    
    · add () - adds the numerical value of the element corresponding to selector1 to the numerical value of the element corresponding to selector2 and then writes the result in the element corresponding to resultSelector.
    
    · subtract () - subtracts the numerical value of the element corresponding to selector1 from the numerical value of the element corresponding to selector2 and then writes the result in the element corresponding to resultSelector.
    
    Input
    
    There will be no input your function must only provide an object.
    
    Output
    
    Your function should return an object that meets the specified requirements.
    
    Constraints
    
    · All commands will always be valid, there will be no nonexistent or incorrect input.
    
    · All selectors will point to single textbox elements.
    
    · Use the given skeleton to solve this problem.

--- 06.Advanced Functions/exercise/07. Simple Calculator/app.js
+++ 06.Advanced Functions/exercise/07. Simple Calculator/app.js
@@ -1,7 +1,23 @@
 function calculator() {
-    // TODO:
+  let selector1, selector2, resultSelector;
+
+  return {
+    init: (s1, s2, rs) => {
+      selector1 = document.querySelector(s1);
+      selector2 = document.querySelector(s2);
+      resultSelector = document.querySelector(rs);
+    },
+    add: () => {
+      const result = Number(selector1.value) + Number(selector2.value);
+      resultSelector.value = result;
+    },
+    subtract: () => {
+      const result = Number(selector1.value) - Number(selector2.value);
+      resultSelector.value = result;
+    },
+  };
 }
 
+const calculate = calculator();
 
-
-
+calculate.init("#num1", "#num2", "#result");
