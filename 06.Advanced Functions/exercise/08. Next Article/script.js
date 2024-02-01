14888a1ed994c1e8ae63fd386a781e04dbbc8ca1
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-01 13:17:22 +0100

    8. Next Article
    Write a JS program that sequentially displays articles on a web page when the user clicks a button. You will receive an array of strings that will initialize the program. You need to return a function that keeps the initial array in its closure and every time it’s called, it takes the first element from the array and displays it on the web page, inside "article", in div with ID "content". If there are no more elements left, your function should do nothing.
    
    Your function will be called automatically, there is no need to attach any event listeners.
    
    Input
    
    You will receive an array of strings.
    
    Output
    
    Return a function that sequentially displays the array elements on the web page.

--- 06.Advanced Functions/exercise/08. Next Article/script.js
+++ 06.Advanced Functions/exercise/08. Next Article/script.js
@@ -1,3 +1,24 @@
 function getArticleGenerator(articles) {
     // TODO
+    let arr=articles;
+    let displayDivRef=document.querySelector("#content")
+return ()=>{
+    if (arr.length) {
+        let currentArticle=arr.shift();
+        let articleEL=document.createElement("article")
+        articleEL.textContent=currentArticle
+      displayDivRef.appendChild(articleEL)
+       console.log(currentArticle);
+    }
+   
 }
+
+}
+
+getArticleGenerator([
+    "Cats are the most popular pet in the United States: There are 88 million pet cats and 74 million dogs.",
+    "A group of cats is called a clowder.",
+    "Cats have over 20 muscles that control their ears.",
+    "A cat has been mayor of Talkeetna, Alaska, for 15 years. His name is Stubbs.",
+    "The world's largest cat measured 48.5 inches long."
+])
