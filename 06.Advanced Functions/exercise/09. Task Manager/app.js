22888911cfd30b4ac94889e3a3290248e75b3913
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-02 18:01:28 +0100

    9. Task Manager
    Use the index.html and app.js files to solve this problem. You have NO permission to directly change the given HTML code (index.html file).
    Your task
    
    Write the missing JavaScript code to make the Task Manager Functionality works as follows:
    
    When the [Add] button is clicked, first you need to validate the inputs. If any of the input fields are empty, the function doesn’t make anything.
    
    After validating the input fields, you need to add the new task (article) in the "Open" section.
    
    The HTML structure looks like this:
    
    The article should have two buttons "Start" and "Delete". Be careful to set the classes for the buttons and the parent-div.
    
    When the [Start] button is clicked, you need to move the Task in the section "In Progress". Be careful with the buttons! The HTML structure looks like this:
    
    When the [Delete] button is clicked, the Task (whole article) should be removed from the HTML.
    
    After clicking the [Finish] button, the Task will be completed, and you should move the article to the section "Complete". The buttons with their parent div-element should be removed.

--- 06.Advanced Functions/exercise/09. Task Manager/app.js
+++ 06.Advanced Functions/exercise/09. Task Manager/app.js
@@ -1,3 +1,99 @@
 function solve() {
-    console.log("//TODO")
+  const taskFieldRef = document.getElementById("task");
+  const descriptionFieldREf = document.getElementById("description");
+  const dueDateFieldRef = document.getElementById("date");
+  const addBtnRef = document.getElementById("add");
+
+  addBtnRef.addEventListener("click", addBtnHandler);
+
+  function addBtnHandler(e) {
+    e.preventDefault();
+    const taskInput = taskFieldRef.value;
+    const descriptionInput = descriptionFieldREf.value;
+    const dueDate = dueDateFieldRef.value;
+
+    if (!taskInput || !descriptionInput || !dueDate) {
+      return;
+    }
+
+    addArticle(taskInput, descriptionInput, dueDate);
+
+    function addArticle(name, description, date) {
+      const openSectionArticlesRef = document.querySelector(
+        "section:nth-child(2) div:nth-child(2)"
+      );
+      let article = document.createElement("article");
+
+      article.innerHTML = `
+        <h3>${name}</h3>
+        <p>Description: ${description}</p>
+        <p>Due Date: ${date}</p>
+        <div class="flex">
+        <button class="green">Start</button>
+        <button class="red">Delete</button>
+        </div>
+        `;
+      openSectionArticlesRef.appendChild(article);
+
+      const startBtn = Array.from(openSectionArticlesRef.querySelectorAll(".green"));
+      const deleteBtn = Array.from(openSectionArticlesRef.querySelectorAll(".red"));
+
+      startBtn.forEach((element) => {
+        element.addEventListener("click", startBtnHandler);
+      });
+
+      deleteBtn.forEach((el) => {
+        el.addEventListener("click", deleteButtonHandlerOrangeSection);
+      });
+
+      function startBtnHandler(e) {
+        const currentArticle = e.target.parentNode.parentNode;
+        const inProgressSectionRef = document.getElementById("in-progress");
+
+        currentArticle.innerHTML = `<h3>${name}</h3>
+        <p>Description: ${description}</p>
+        <p>Due Date: ${date}</p>
+        <div class="flex">
+        <button class="red">Delete</button>
+        <button class="orange">Finish</button>
+        </div>`;
+        inProgressSectionRef.appendChild(currentArticle);
+
+        const deleteButtonsYellowSection =Array.from(inProgressSectionRef.querySelectorAll(".red"));
+          
+        const finishBtn = Array.from(inProgressSectionRef.querySelectorAll(".orange"));
+
+        deleteButtonsYellowSection.forEach((el) => {
+          el.addEventListener("click", deleteBtnYellSectHandler);
+        });
+        finishBtn.forEach((el) => {
+          el.addEventListener("click", finishBtnHandler);
+        });
+
+        function deleteBtnYellSectHandler(e) {
+          const deleteCurrentArticle = e.target.parentNode.parentNode;
+          inProgressSectionRef.removeChild(deleteCurrentArticle);
+        }
+
+        function finishBtnHandler(e) {
+          let finishArticle = e.target.parentNode.parentNode;
+
+          finishArticle.innerHTML = `
+         <h3>${name}</h3>
+         <p>Description: ${description}</p>
+         <p>Due Date: ${date}</p>`;
+          const completeSectRef = document.querySelector(
+            "section:nth-child(4) div:nth-child(2)"
+          );
+
+          completeSectRef.appendChild(finishArticle);
+        }
+      }
+
+      function deleteButtonHandlerOrangeSection(e) {
+        const currentArticle = e.target.parentNode.parentNode;
+        openSectionArticlesRef.removeChild(currentArticle);
+      }
+    }
+  }
 }
