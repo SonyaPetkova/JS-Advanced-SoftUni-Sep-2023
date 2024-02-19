29b9e12fc2cf6be54b1fee3e0516f0cf37aedcc9
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-19 10:11:14 +0100

    5. Notification
    Write a JS function that receives a string message and displays it inside a div with id "notification. The div starts hidden and when the function is called, reveal it. After the user clicks on it, hide the div. In the example document, a notification is shown when you click on the button ["Get notified"].

--- 07.Unit Testing and Error Handling/exersice/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/05.Notification/app.js
+++ 07.Unit Testing and Error Handling/exersice/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/05.Notification/app.js
@@ -1,4 +1,13 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  console.log("todo");
+  const notificationRef = document.getElementById("notification");
+  console.log(notificationRef);
+
+  notificationRef.textContent = message;
+  notificationRef.style.display = "block";
+
+  notificationRef.addEventListener(
+    "click",
+    () => (notificationRef.style.display = "none")
+  );
 }
