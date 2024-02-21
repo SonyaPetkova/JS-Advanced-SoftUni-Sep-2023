696bed885988575df7341e0feccff560a1ebbc40
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-21 11:22:54 +0100

    2. Data Class
    Write a class Request that holds data about an HTTP request. It has the following properties:
    
    · method (String)
    
    · uri (String)
    
    · version (String)
    
    · message (String)
    
    · response (String)
    
    · fulfilled (Boolean)
    
    The first four properties (method, uri, version, message) are set through the constructor, in the listed order. The response property is initialized to undefined and the fulfilled property is initially set to false.
    
    Constraints
    
    · The constructor of your class will receive valid parameters.
    
    · Submit the class definition as is, without wrapping it in any function.

--- 08.Classes/exercise/rectangle.js
+++ 08.Classes/exercise/rectangle.js
@@ -1,18 +1,14 @@
 class Rectangle {
-  constructor(width,height,color) {
-
-this.width=width,
-this.height=height,
-this.color=color
-
+  constructor(width, height, color) {
+    (this.width = width), (this.height = height), (this.color = color);
   }
 
-  calcArea(){
-    return this.width*this.height
+  calcArea() {
+    return this.width * this.height;
   }
 }
 
-let rect = new Rectangle(4, 5, 'Red');
+let rect = new Rectangle(4, 5, "Red");
 
 console.log(rect.width);
 
