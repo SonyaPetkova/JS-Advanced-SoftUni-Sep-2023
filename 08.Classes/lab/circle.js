21478c2d64d56dd9d2a94952e1f7494ae8c0d720
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-21 10:28:06 +0100

    3. Circle
    Write a class that represents a Circle. It has only one data property - its radius, and it is set through the constructor. The class needs to have getter and setter methods for its diameter - the setter needs to calculate the radius and change it and the getter needs to use the radius to calculate the diameter and return it.
    
    The circle also has a getter area(), which calculates and returns its area.
    
    Input
    
    The constructor function and diameter setter will receive valid parameters.
    
    Output
    
    The diameter() and area() getters should return numbers.
    
    Submit the class definition as is, without wrapping it in any function.

---
+++ 08.Classes/lab/circle.js
@@ -0,0 +1,38 @@
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  get radius() {
+    return this._radius;
+  }
+  set radius(r) {
+    return (this._radius = r);
+  }
+
+  get diameter() {
+    return this.radius * 2;
+  }
+  set diameter(d) {
+    return (this.radius = d / 2);
+  }
+  get area() {
+    return Math.PI * Math.pow(this.radius, 2)
+  }
+}
+
+let c = new Circle(2);
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
+
+c.diameter = 1.6;
+
+console.log(`Radius: ${c.radius}`);
+
+console.log(`Diameter: ${c.diameter}`);
+
+console.log(`Area: ${c.area}`);
