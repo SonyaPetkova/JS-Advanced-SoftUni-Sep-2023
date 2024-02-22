e63bfc86e8cb61632acfd56d69c4c0e1b9858d8b
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-22 12:10:34 +0100

    6. Company
    class Company { // TODO: implement this class... }
    
    Write a class Company, which following these requirements:
    
    The constructor takes no parameters:
    
    You could initialize an object:
    
    · departments - empty object
    
    addEmployee({name}, {salary}, {position}, {department})
    
    This function should add a new employee to the department with the given name.
    
    · If one of the passed parameters is an empty string (""), undefined or null, this function should throw an error with the following message: "Invalid input!"
    
    · If salary is less than 0, this function should throw an error with the following message: "Invalid input!"
    
    · If the new employee is hired successfully, you should add him into the departments object with the current name of the department and return the following message: `New employee is hired. Name: {name}. Position: {position}`
    
    bestDepartment()
    
    This function should return the department with the highest average salary rounded to the second digit after the decimal point and its employees sorted by their salary by descending order and by their name in ascending order as a second criterion:
    
    `Best Department is: {best department's name}
    
    Average salary: {best department's average salary}
    
    {employee1} {salary} {position}
    
    {employee2} {salary} {position}
    
    {employee3} {salary} {position}
    
    …`
    
    Submission
    
    Submit only the Company class definition.

---
+++ 08.Classes/exercise/company.js
@@ -0,0 +1,71 @@
+class Company {
+  constructor() {
+    this.departments = {};
+  }
+
+  addEmployee(name, salary, position, department) {
+    if (!name || !salary ||salary===""< 0 || !position || !department) {
+      throw new Error("Invalid input!");
+    }
+
+    if (!this.departments.hasOwnProperty(department)) {
+      this.departments[department] = [];
+      this.departments[department].averageSalary = [];
+    }
+
+    this.departments[department].push({ name, salary, position });
+    this.departments[department].averageSalary.push(salary);
+
+    return `New employee is hired. Name: ${name}. Position: ${position}`;
+  }
+
+  bestDepartment() {
+    for (const department in this.departments) {
+      let average =
+        this.departments[department].averageSalary.reduce(
+          (acc, salary) => acc + salary,
+          0
+        ) / this.departments[department].averageSalary.length;
+
+      this.departments[department].averageSalary = average;
+    }
+
+    let bestDep = [0, null, ""];
+
+    for (const department in this.departments) {
+      let sortedEmployees = this.departments[department];
+      sortedEmployees.sort((a, b) => {
+        if (b.salary !== a.salary) {
+          return b.salary - a.salary;
+        } else {
+          return a.name.localeCompare(b.name);
+        }
+      });
+
+      if (bestDep[0] < this.departments[department].averageSalary) {
+        bestDep[0] = this.departments[department].averageSalary;
+        bestDep[1] = sortedEmployees;
+        bestDep[2] = department;
+      }
+    }
+
+    let output = `Best Department is: ${bestDep[2]}\n`;
+    output += `Average salary: ${bestDep[0].toFixed(2)}`;
+
+    for (const element of bestDep[1]) {
+      output += `\n${element.name} ${element.salary} ${element.position}`;
+    }
+
+    return output;
+  }}
+
+let c = new Company();
+
+c.addEmployee("Stanimir", "", "engineer", "Construction");
+c.addEmployee("Pesho", 1500, "electrical engineer", "Construction");
+c.addEmployee("Slavi", 500, "dyer", "Construction");
+c.addEmployee("Stan", 2000, "architect", "Construction");
+c.addEmployee("Stanimir", 1200, "digital marketing manager", "Marketing");
+c.addEmployee("Pesho", 1000, "graphical designer", "Marketing");
+c.addEmployee("Gosho", 1350, "HR", "Human resources");
+console.log(c.bestDepartment());
