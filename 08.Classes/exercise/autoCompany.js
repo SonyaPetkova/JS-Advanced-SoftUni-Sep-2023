cd208bf75a997a7dbd396f9ef94825c7b135484e
SonyaPetkova <sonya.petkovaa@gmail.com>
2024-02-26 15:36:54 +0100

    9. Auto-Engineering Company
    You are tasked to create a register for a company that produces cars. You need to store how many cars have been produced from a specific model of a specific brand.
    
    Input
    
    The input comes as array of strings. Each element holds information in the following format:
    
    "{carBrand} | {carModel} | {producedCars}"
    
    The carBrand and carModel are strings, the producedCars are numbers. If the carBrand you’ve received already exists, just add the new carModel to it with the produced cars as its value. If even the carModel exists, just add the given value to the current one.
    
    Output
    
    As output, you need to print - for every car brand, the car models, and a number of cars produced from that model. The output format is:
    
    `{carBrand}
    
    ###{carModel} -> {producedCars}
    
    ###{carModel2} -> {producedCars}
    
    ...`
    
    The order of printing is the order in which the brands and models first appear in the input. The first brand in the input should be the first printed and so on. For each brand, the first model received from that brand, should be the first printed and so on.

---
+++ 08.Classes/exercise/autoCompany.js
@@ -0,0 +1,48 @@
+function carCompany(arr) {
+    let carProduced=new Map();
+    for (const line of arr) {
+
+        let[brand,model,quantity]=line.split(" | ");
+        if(!carProduced.has(brand)){
+            carProduced.set(brand,new Map([[model, Number(quantity)]]))
+        }else{
+           let currentModel=carProduced.get(brand) ;
+          
+           if (!currentModel.has(model)) {
+            currentModel.set(model,Number(quantity))
+           }else{
+            currentModel.set(model, currentModel.get(model) + Number(quantity));
+           }
+        }
+        
+        
+    }
+
+    for (const brand of carProduced) {
+        console.log(brand[0]);
+        for (const [model,quantity] of brand[1]) {
+            console.log(`###${model} -> ${quantity}`);
+        }
+       
+    }
+
+}
+carCompany(['Audi | Q7 | 1000',
+
+'Audi | Q6 | 100',
+
+'BMW | X5 | 1000',
+
+'BMW | X6 | 100',
+
+'Citroen | C4 | 123',
+
+'Volga | GAZ-24 | 1000000',
+
+'Lada | Niva | 1000000',
+
+'Lada | Jigula | 1000000',
+
+'Citroen | C4 | 22',
+
+'Citroen | C5 | 10'])
